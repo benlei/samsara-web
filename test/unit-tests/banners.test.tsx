9eb4848e7489e9ee0d7ba7d195b71908586a26c5
Benjamin Lei <benlei.cse@gmail.com>
2022-12-25 03:18:12 -0800

    additional tests

--- test/unit-tests/banners.test.tsx
+++ test/unit-tests/banners.test.tsx
@@ -7,4 +7,17 @@ describe('getVersionParts', () => {
         const versionParts = getVersionParts(banners.characters["5"])
         expect(versionParts.length).toBeGreaterThanOrEqual(15)
     });
+
+    it('should end with 1.0', async () => {
+        const banners = require('@/data/banners.json')
+        const versionParts = getVersionParts(banners.characters["5"])
+        expect(versionParts[versionParts.length - 1].version).toEqual("1.0")
+        expect(versionParts[versionParts.length - 1].parts).toEqual(2)
+    });
+
+    it('should have 3 parts for 1.3', async () => {
+        const banners = require('@/data/banners.json')
+        const versionParts = getVersionParts(banners.characters["5"])
+        expect(versionParts[versionParts.length - 4].parts).toEqual(3)
+    });
 });
