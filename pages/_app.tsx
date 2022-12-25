d7988615acf75e8c177fdb051d209619d91a59a6
Benjamin Lei <benlei.cse@gmail.com>
2022-12-24 21:03:00 -0800

    saving progress, copied semantic ui's example page for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import 'semantic-ui-css/semantic.min.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,108 +14,83 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
+      <main>
+        <div className="ui fixed inverted menu">
+          <div className="ui container">
+            <a href="#" className="header item">
+                Project Name
             </a>
+            <a href="#" className="item">Home</a>
+            <div className="ui simple dropdown item">
+              Dropdown <i className="dropdown icon"></i>
+              <div className="menu">
+                <a className="item" href="#">Link Item</a>
+                <a className="item" href="#">Link Item</a>
+                <div className="divider"></div>
+                <div className="header">Header Item</div>
+                <div className="item">
+                  <i className="dropdown icon"></i>
+                  Sub Menu
+                  <div className="menu">
+                    <a className="item" href="#">Link Item</a>
+                    <a className="item" href="#">Link Item</a>
+                  </div>
+                </div>
+                <a className="item" href="#">Link Item</a>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+        <div className="ui main text container">
+          <h1 className="ui header">Semantic UI Fixed Template</h1>
+          <p>This is a basic fixed menu template using fixed size containers.</p>
+          <p>A text container is used for the main container, which is useful for single column layouts</p>
         </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className="ui inverted vertical footer segment">
+          <div className="ui center aligned container">
+            <div className="ui stackable inverted divided grid">
+              <div className="three wide column">
+                <h4 className="ui inverted header">Group 1</h4>
+                <div className="ui inverted link list">
+                  <a href="#" className="item">Link One</a>
+                  <a href="#" className="item">Link Two</a>
+                  <a href="#" className="item">Link Three</a>
+                  <a href="#" className="item">Link Four</a>
+                </div>
+              </div>
+              <div className="three wide column">
+                <h4 className="ui inverted header">Group 2</h4>
+                <div className="ui inverted link list">
+                  <a href="#" className="item">Link One</a>
+                  <a href="#" className="item">Link Two</a>
+                  <a href="#" className="item">Link Three</a>
+                  <a href="#" className="item">Link Four</a>
+                </div>
+              </div>
+              <div className="three wide column">
+                <h4 className="ui inverted header">Group 3</h4>
+                <div className="ui inverted link list">
+                  <a href="#" className="item">Link One</a>
+                  <a href="#" className="item">Link Two</a>
+                  <a href="#" className="item">Link Three</a>
+                  <a href="#" className="item">Link Four</a>
+                </div>
+              </div>
+              <div className="seven wide column">
+                <h4 className="ui inverted header">Footer Header</h4>
+                <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+              </div>
+            </div>
+            <div className="ui inverted section divider"></div>
+              <div className="ui horizontal inverted small divided link list">
+                <a className="item" href="#">Site Map</a>
+                <a className="item" href="#">Contact Us</a>
+                <a className="item" href="#">Terms and Conditions</a>
+                <a className="item" href="#">Privacy Policy</a>
+              </div>
+          </div>
         </div>
       </main>
     </>
