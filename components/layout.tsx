7f2fe5fca01cbb73ea21d2e93f943d1d02d48f23
Benjamin Lei <benlei.cse@gmail.com>
2022-12-27 00:22:18 -0800

    added a favicon, updating headers and what not

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,7 +9,10 @@ export default function Layout({children}: React.PropsWithChildren) {
     return (
         <>
             <Head>
-                <title>Geshin Impact - Banner History | Samsara</title>
+                <title>Samsara</title>
+                <link rel="icon" href="/favicon.ico" />
+                <meta name="description" content="Genshin Impact in a Loop. Character banner, weapon banners, resin, and potentially more to come!" />
+                <meta name="keywords" content="Genshin, Genshin Impact, banners, characters, weapons, resin, samsara, repeat, loop, history, rundown" />
             </Head>
             <main>
                 <Navbar></Navbar>
