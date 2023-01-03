6aa57209acbd5fef4aa2e4968cbf8c8573bc1f3d
Benjamin Lei <benlei.cse@gmail.com>
2023-01-02 19:56:17 -0800

    added unfixed rotation tests

--- test/unit-tests/artifact-presets.test.tsx
+++ test/unit-tests/artifact-presets.test.tsx
@@ -1,7 +1,7 @@
 import {RotationStorage} from "@/artifacts/types";
 import {dateStringAsDate, getRotationIndexAndDay} from "@/artifacts/presets";
 
-const storage: RotationStorage = {
+const FIXED_STORAGE: RotationStorage = {
     active: 0,
     presets: [
         {
@@ -18,12 +18,29 @@ const storage: RotationStorage = {
     ]
 }
 
+const UNFIXED_STORAGE: RotationStorage = {
+    active: 0,
+    presets: [
+        {
+            name: "blah",
+            fixed: false,
+            fixedDays: 3,
+            date: "2023-01-01",
+            rotations: [
+                {domain: "", characters: [], note: "", days: 3},
+                {domain: "", characters: [], note: "", days: 7},
+                {domain: "", characters: [], note: "", days: 14},
+            ],
+        }
+    ]
+}
+
 describe('getRotationIndexAndDay()', () => {
     it('should correctly calculate current rotation + day for fixed rotations of same day', async () => {
         const endDate = dateStringAsDate("2023-01-01")
 
         // 32 % 9 = 5, or index 1 day 2
-        expect(getRotationIndexAndDay(storage, 0, endDate)).toEqual({
+        expect(getRotationIndexAndDay(FIXED_STORAGE, 0, endDate)).toEqual({
             index: 0,
             day: 1,
         })
@@ -33,7 +50,7 @@ describe('getRotationIndexAndDay()', () => {
         const endDate = dateStringAsDate("2023-01-05")
 
         // 32 % 9 = 5, or index 1 day 2
-        expect(getRotationIndexAndDay(storage, 0, endDate)).toEqual({
+        expect(getRotationIndexAndDay(FIXED_STORAGE, 0, endDate)).toEqual({
             index: 1,
             day: 2,
         })
@@ -42,8 +59,7 @@ describe('getRotationIndexAndDay()', () => {
     it('should correctly calculate current rotation + day for fixed rotations 3rd rotation', async () => {
         const endDate = dateStringAsDate("2023-01-09")
 
-        // 32 % 9 = 5, or index 1 day 2
-        expect(getRotationIndexAndDay(storage, 0, endDate)).toEqual({
+        expect(getRotationIndexAndDay(FIXED_STORAGE, 0, endDate)).toEqual({
             index: 2,
             day: 3,
         })
@@ -52,10 +68,45 @@ describe('getRotationIndexAndDay()', () => {
     it('should correctly calculate current rotation + day for fixed rotations', async () => {
         const endDate = dateStringAsDate("2023-02-01")
 
-        // 32 % 9 = 5, or index 1 day 2
-        expect(getRotationIndexAndDay(storage, 0, endDate)).toEqual({
+        expect(getRotationIndexAndDay(FIXED_STORAGE, 0, endDate)).toEqual({
             index: 1,
             day: 2,
+        })
+    });
+
+    it('should correctly calculate current rotation + day for unfixed rotations of same day', async () => {
+        const endDate = dateStringAsDate("2023-01-01")
+
+        expect(getRotationIndexAndDay(UNFIXED_STORAGE, 0, endDate)).toEqual({
+            index: 0,
+            day: 1,
+        })
+    });
+
+    it('should correctly calculate current rotation + day for unfixed rotations 2nd rotation', async () => {
+        const endDate = dateStringAsDate("2023-01-05")
+
+        expect(getRotationIndexAndDay(UNFIXED_STORAGE, 0, endDate)).toEqual({
+            index: 1,
+            day: 2,
+        })
+    });
+
+    it('should correctly calculate current rotation + day for unfixed rotations 3rd rotation', async () => {
+        const endDate = dateStringAsDate("2023-01-14")
+
+        expect(getRotationIndexAndDay(UNFIXED_STORAGE, 0, endDate)).toEqual({
+            index: 2,
+            day: 4,
+        })
+    });
+
+    it('should correctly calculate current rotation + day for unfixed rotations', async () => {
+        const endDate = dateStringAsDate("2023-02-01")
+
+        expect(getRotationIndexAndDay(UNFIXED_STORAGE, 0, endDate)).toEqual({
+            index: 1,
+            day: 5,
         })
     });
 })
