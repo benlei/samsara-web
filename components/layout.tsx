bf9b176d0fc5502f428b455df33a3449fe12faeb
Benjamin Lei <9943953+benlei@users.noreply.github.com>
2023-01-07 12:08:59 -0800

    Scale change (#23)
    * try the default viewport
    
    * try the default viewport

--- components/layout.tsx
+++ components/layout.tsx
@@ -13,8 +13,7 @@ export default function Layout({children}: React.PropsWithChildren) {
                       content="Genshin Impact in a Loop. Character banner, weapon banners, resin, and potentially more to come!"/>
                 <meta name="keywords"
                       content="Genshin, Genshin Impact, banners, characters, weapons, resin, samsara, repeat, loop, history, rundown"/>
-                <meta name="viewport"
-                      content="width=device-width, initial-scale=.5, maximum-scale=12.0, minimum-scale=.25, user-scalable=yes"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <script>
                     {/*to prevent Firefox FOUC, this must be here*/}
                     0
