e6a8519c5411a38129d7c43b1fb0850e817fe0e8
Benjamin Lei <9943953+benlei@users.noreply.github.com>
2023-01-07 20:14:41 -0800

    closes #20 - manual cloudflare deploy (#25)
    * add in deploy job
    
    * add in caching
    
    * add in node_modules as well to cache
    
    * needs runs on
    
    * report list even on ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { PlaywrightTestConfig, devices } from '@playwright/test'
+import {devices, PlaywrightTestConfig} from '@playwright/test'
 import path from 'path'
 
 const PORT = process.env.PORT || 8788
@@ -8,57 +8,57 @@ const baseURL = `http://localhost:${PORT}`
 
 // Reference: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
-  // Concise 'dot' for CI, default 'list' when running locally
-  reporter: process.env.CI ? 'dot' : 'list',
-  // Timeout per test
-  timeout: 30 * 1000,
-  // Test directory
-  testDir: path.join(__dirname, 'test/e2e'),
-  // If a test fails, retry it additional 2 times
-  retries: 0,
-  // Artifacts folder where screenshots, videos, and traces are stored.
-  outputDir: path.join(__dirname, 'test-results'),
+    // Concise 'dot' for CI, default 'list' when running locally
+    reporter: 'list',
+    // Timeout per test
+    timeout: 30 * 1000,
+    // Test directory
+    testDir: path.join(__dirname, 'test/e2e'),
+    // If a test fails, retry it additional 2 times
+    retries: 0,
+    // Artifacts folder where screenshots, videos, and traces are stored.
+    outputDir: path.join(__dirname, 'test-results'),
 
-  // Run your local dev server before starting the tests:
-  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
-  webServer: {
-    command: 'npx wrangler pages dev .vercel/output/static --compatibility-flags=streams_enable_constructors',
-    url: baseURL,
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
-
-  use: {
-    // Use baseURL so to make navigations relative.
-    // More information: https://playwright.dev/docs/api/class-testoptions#test-options-base-url
-    baseURL,
-
-    screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
-
-    // All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
-    // contextOptions: {
-    //   ignoreHTTPSErrors: true,
-    // },
-  },
-
-  projects: [
-    {
-      name: 'Desktop Chrome',
-      use: {
-        ...devices['Desktop Chrome'],
-      },
+    // Run your local dev server before starting the tests:
+    // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
+    webServer: {
+        command: 'npx wrangler pages dev .vercel/output/static --compatibility-flags=streams_enable_constructors',
+        url: baseURL,
+        timeout: 120 * 1000,
+        reuseExistingServer: !process.env.CI,
     },
-    {
-      name: 'Mobile Chrome',
-      use: {
-        ...devices['Pixel 5'],
-      },
+
+    use: {
+        // Use baseURL so to make navigations relative.
+        // More information: https://playwright.dev/docs/api/class-testoptions#test-options-base-url
+        baseURL,
+
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+
+        // All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
+        // contextOptions: {
+        //   ignoreHTTPSErrors: true,
+        // },
     },
-    {
-      name: 'Mobile Safari',
-      use: devices['iPhone 12'],
-    },
-  ],
+
+    projects: [
+        {
+            name: 'Desktop Chrome',
+            use: {
+                ...devices['Desktop Chrome'],
+            },
+        },
+        {
+            name: 'Mobile Chrome',
+            use: {
+                ...devices['Pixel 5'],
+            },
+        },
+        {
+            name: 'Mobile Safari',
+            use: devices['iPhone 12'],
+        },
+    ],
 }
 export default config
