9e392c6e7d249c3fc2d5f5ee22dd473297a13f78
Benjamin Lei <benlei.cse@gmail.com>
2023-01-16 01:15:20 -0800

    realized i needed aloy + travler...
    also test for getCharacters was in wrong directory

--- characters/characters.tsx
+++ characters/characters.tsx
@@ -3,14 +3,29 @@ import {getImageFromName} from "@/format/image";
 import _ from "lodash";
 
 export function getCharacters(data: string[]): Characters {
-    return _.chain(data)
-        .reduce((result: Characters, value) => {
-            result[value] = {
-                name: value,
-                image: getImageFromName(value),
-            }
+    function addOneOffs(result: Characters): Characters {
+        result['Traveler'] = {
+            name: 'Traveler',
+            image: 'Traveler',
+        }
 
-            return result
-        }, {})
-        .value()
+        result['Aloy'] = {
+            name: 'Aloy',
+            image: 'Aloy',
+        }
+        return result
+    }
+
+    return addOneOffs(
+        _.chain(data)
+            .reduce((result: Characters, value) => {
+                result[value] = {
+                    name: value,
+                    image: getImageFromName(value),
+                }
+
+                return result
+            }, {})
+            .value()
+    )
 }
--- test/unit-tests/characters.test.tsx
+++ test/unit-tests/characters.test.tsx
@@ -1,14 +1,16 @@
 import {getCharacters} from "@/characters/characters";
 
 describe('getCharacters()', () => {
-    it('should have same number of elements as input', async () => {
+    it('should include one-offs', async () => {
         const data: string[] = [
             ...Object.keys(require('@/data/banners.json').characters['5']),
             ...Object.keys(require('@/data/banners.json').characters['4']),
         ]
         const characters = getCharacters(data)
 
-        expect(data.length).toEqual(Object.keys(characters).length)
+        expect(data.length).toEqual(Object.keys(characters).length - 2)
+        expect(Object.keys(characters)).toContain('Traveler')
+        expect(Object.keys(characters)).toContain('Aloy')
     });
 
     it('should have Yae Miko', async () => {
