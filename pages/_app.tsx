53cc5fcd0cf6be99f73b94bbb8eab7537cf9797b
Benjamin Lei <benlei.cse@gmail.com>
2023-02-18 00:11:56 -0800

    use ga4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,11 @@ import 'semantic-ui-css/semantic.min.css'
 import React from 'react';
 import App, {AppProps} from 'next/app';
 import Layout from '@/components/layout';
-import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 ReactGA.initialize(process.env.NEXT_PUBLIC_ANALYTICS_ID ? process.env.NEXT_PUBLIC_ANALYTICS_ID : 'fake');
 
 class MyApp extends App<AppProps> {
-    componentDidMount = () => ReactGA.pageview(window.location.pathname + window.location.search);
-    componentDidUpdate = () => ReactGA.pageview(window.location.pathname + window.location.search);
-
     render() {
         const {Component, pageProps} = this.props;
 
