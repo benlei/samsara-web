7a25b2a454986db6e15713d6637d7562162c5250
Benjamin Lei <benlei.cse@gmail.com>
2023-03-28 21:31:58 -0700

    #22 - compiled semantic ui css stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import '@/styles/globals.css'
-import 'semantic-ui-css/semantic.min.css'
+import '@/styles/semantic.css'
 import React from 'react';
 import App, {AppProps} from 'next/app';
 import Layout from '@/components/layout';
 import ReactGA from 'react-ga4';
+import {ThemeProvider} from 'next-themes';
 
 ReactGA.initialize(process.env.NEXT_PUBLIC_ANALYTICS_ID ? process.env.NEXT_PUBLIC_ANALYTICS_ID : 'fake');
 
@@ -15,8 +16,9 @@ class MyApp extends App<AppProps> {
             <>
                 {/* Add any global styles or other components here */}
                 <Layout>
-                    <Component {...pageProps} />
-
+                    <ThemeProvider defaultTheme={'light'} enableSystem={false}>
+                        <Component {...pageProps} />
+                    </ThemeProvider>
                 </Layout>
             </>
         );
