e28f5f5cd7ae937b69b747a7c21bdd3a1957e37f
Benjamin Lei <benlei.cse@gmail.com>
2023-04-08 13:42:47 -0700

    update config to skip test stuff

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    webpack: (config, { dev }) => {
+        config.module.rules.push(
+            {
+                test: /\.spec.js$/,
+                loader: 'ignore-loader'
+            },
+            {
+                test: /playwright\.config\.ts$/,
+                loader: 'ignore-loader'
+            }
+        );
+        return config;
+    }
 }
 
 module.exports = nextConfig
