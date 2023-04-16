997db427fda10f8a062cca3722a68532ec07e63b
Benjamin Lei <benlei.cse@gmail.com>
2023-04-16 00:33:49 -0700

    more adjustments for mobile

--- components/summary/stat/LeaderboardCounterSummary.tsx
+++ components/summary/stat/LeaderboardCounterSummary.tsx
@@ -12,7 +12,9 @@ type Properties = {
     featuredList: Featured[]
     type: string
     date: string
-    defaultOrder: Order
+    order: Order
+    sortBy: string
+    triggerSort: (newSort: string) => void
     counter: (versionParts: VersionParts[], featuredList: Featured[], date: string) => LeaderboardSummary[]
 }
 
@@ -22,20 +24,13 @@ export default function LeaderboardCounterSummary(
         type,
         counter,
         date,
-        defaultOrder,
+        sortBy,
+        order,
+        triggerSort,
     }: Properties
 ) {
-    function triggerSort(newSort: string) {
-        if (sortBy != newSort) {
-            setSortBy(newSort)
-        } else {
-            setOrder(order == 'asc' ? 'desc' : 'asc')
-        }
-    }
 
     const [now, setNow] = useState(date as string)
-    const [sortBy, setSortBy] = useState('days')
-    const [order, setOrder] = useState(defaultOrder)
     const summary = _.chain(counter(getVersionPartsFromFeaturedList(featuredList, 'asc'), featuredList, now))
         .orderBy([
             (b) => sortBy === 'patches' ? b.patches : (sortBy === 'banners' ? b.banners : b.days),
@@ -52,31 +47,22 @@ export default function LeaderboardCounterSummary(
                 <Table.Row>
                     <Table.HeaderCell colSpan={2} className={'active'}>Featured</Table.HeaderCell>
                     <Table.HeaderCell
-                        className={'clickable sortable-column'}
+                        className={clsx('clickable sortable-column', {desktop: sortBy !== 'days'})}
                         onClick={() => triggerSort('days')}
                     >
-                        <span className={'desktop'}>Days <Icon name={'sort'}
-                                                               className={clsx({hidden: sortBy !== 'days'})}/></span>
-                        <span className={'mobile'} style={{display: 'none'}}>D <Icon name={'sort'}
-                                                                                     className={clsx({hidden: sortBy !== 'days'})}/></span>
+                        Days <Icon name={'sort'} className={clsx({hidden: sortBy !== 'days'}, 'desktop')}/>
                     </Table.HeaderCell>
                     <Table.HeaderCell
-                        className={'clickable sortable-column'}
+                        className={clsx('clickable sortable-column', {desktop: sortBy !== 'banners'})}
                         onClick={() => triggerSort('banners')}
                     >
-                        <span className={'desktop'}>Banners <Icon name={'sort'}
-                                                                  className={clsx({hidden: sortBy !== 'banners'})}/></span>
-                        <span className={'mobile'} style={{display: 'none'}}>B <Icon name={'sort'}
-                                                                                     className={clsx({hidden: sortBy !== 'banners'})}/></span>
+                        Banners <Icon name={'sort'} className={clsx({hidden: sortBy !== 'banners'}, 'desktop')}/>
                     </Table.HeaderCell>
                     <Table.HeaderCell
-                        className={'clickable sortable-column'}
+                        className={clsx('clickable sortable-column', {desktop: sortBy !== 'patches'})}
                         onClick={() => triggerSort('patches')}
                     >
-                        <span className={'desktop'}>Patches <Icon name={'sort'}
-                                                                  className={clsx({hidden: sortBy !== 'patches'})}/></span>
-                        <span className={'mobile'} style={{display: 'none'}}>P <Icon name={'sort'}
-                                                                                     className={clsx({hidden: sortBy !== 'patches'})}/></span>
+                        Patches <Icon name={'sort'} className={clsx({hidden: sortBy !== 'patches'}, 'desktop')}/>
                     </Table.HeaderCell>
                 </Table.Row>
             </Table.Header>
@@ -103,13 +89,13 @@ export default function LeaderboardCounterSummary(
                         <Table.Cell verticalAlign={'top'}>
                             <Header as={'div'} size={'small'}>{s.name}</Header>
                         </Table.Cell>
-                        <Table.Cell verticalAlign={'top'}>
+                        <Table.Cell verticalAlign={'top'} className={clsx({desktop: sortBy !== 'days'})}>
                             {s.days}
                         </Table.Cell>
-                        <Table.Cell verticalAlign={'top'}>
+                        <Table.Cell verticalAlign={'top'} className={clsx({desktop: sortBy !== 'banners'})}>
                             {s.banners}
                         </Table.Cell>
-                        <Table.Cell verticalAlign={'top'}>
+                        <Table.Cell verticalAlign={'top'} className={clsx({desktop: sortBy !== 'patches'})}>
                             {s.patches}
                         </Table.Cell>
                     </Table.Row>
