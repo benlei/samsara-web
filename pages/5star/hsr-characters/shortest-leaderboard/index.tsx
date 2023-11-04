7a6ab3b34176aaca127204ac21f26fc95c385283
Benjamin Lei <benlei.cse@gmail.com>
2023-11-04 15:55:46 -0700

    adding 5 star chars

---
+++ pages/5star/hsr-characters/shortest-leaderboard/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import Head from "next/head";
+import {Featured} from "@/banners/types";
+import YAML from "yaml";
+import fs from "fs";
+import path from "path";
+import ShortestLeaderboardPage from "@/components/summary/ShortestLeaderboardPage";
+
+export async function getStaticProps() {
+    dayjs.extend(utc);
+    return {
+        props: {
+            featuredList: YAML.parse(fs.readFileSync(path.resolve('./public/data/hsr-banners.yaml'), 'utf8')).fiveStarCharacters,
+            date: dayjs.utc().toISOString().substring(0, 10)
+        },
+    };
+}
+
+
+export default function FiveStarCharacterSummary(props: { featuredList: Featured[], date: string }) {
+    return (
+        <>
+            <Head>
+                <title>5&#x2605; HSR Character Shortest Rerun Leaderboard - Samsara</title>
+            </Head>
+            <ShortestLeaderboardPage
+                title={<>5&#x2605; HSR Character Shortest Rerun Leaderboard</>}
+                data={props}
+                type={'hsr-characters'}
+            />
+        </>
+    )
+}
