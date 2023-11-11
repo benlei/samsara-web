35b22f54043fe3bcf2c506a95d1ef7debb0b2215
Benjamin Lei <benlei.cse@gmail.com>
2023-11-11 00:05:13 -0800

    fixing up e2e test

--- test/e2e/history.spec.ts
+++ test/e2e/history.spec.ts
@@ -1,7 +1,12 @@
-import {expect, test} from '@playwright/test'
+import { expect, test } from "@playwright/test";
 
-test('should be able to see history page', async ({page}) => {
-    await page.goto('/5star/characters')
-    expect(await page.$$("text='3.0'")).toHaveLength(2)
-    expect(await page.$$("text='35'")).not.toHaveLength(0)
-})
+test("should be able to see history page", async ({ page }) => {
+  await page.goto("/5star/characters");
+  expect(await page.$$("text='3.0'")).toHaveLength(2);
+  expect(await page.$$("text='35'")).not.toHaveLength(0);
+});
+
+test("should be able to see homepage", async ({ page }) => {
+  await page.goto("/");
+  expect(page.getByText("Latest")).toBeVisible();
+});
