d1e4337c85b3d0b2cb946a0070a271451879d71e
Benjamin Lei <benlei.cse@gmail.com>
2023-11-16 00:48:32 -0800

    first convert these two components to funtion based components

--- components/history/BannerTable.tsx
+++ components/history/BannerTable.tsx
@@ -1,117 +1,122 @@
-import React from "react";
-import {BannerFilterSortOptions, BannerHistoryDataset, FeaturedHistory} from "@/banners/types";
-import {Table} from "semantic-ui-react";
+import React, { useState } from "react";
+import {
+  BannerFilterSortOptions,
+  BannerHistoryDataset,
+  FeaturedHistory,
+} from "@/banners/types";
+import { Table } from "semantic-ui-react";
 import _ from "lodash";
 import HistoryHeader from "@/components/history/HistoryHeader";
 import HistoryFooter from "@/components/history/HistoryFooter";
 import HistoryRow from "@/components/history/HistoryRow";
-import {getFilterFunction} from "@/banners/summary";
-import getVersionParts, {versionToNumber} from "@/banners/version";
-import {getRundowns} from "@/banners/rundown";
-
+import { getFilterFunction } from "@/banners/summary";
+import getVersionParts, { versionToNumber } from "@/banners/version";
+import { getRundowns } from "@/banners/rundown";
 
 type BannerRundownProps = {
-    bannerType: string
-    featuredList: FeaturedHistory[]
-    dataset: BannerHistoryDataset
-} & BannerFilterSortOptions
+  bannerType: string;
+  featuredList: FeaturedHistory[];
+  dataset: BannerHistoryDataset;
+} & BannerFilterSortOptions;
 
-type BannerRundownState = {
-    filterText: string
-}
+export default function BannerTable({
+  bannerType,
+  featuredList,
+  dataset,
+  sortBy,
+  order,
+}: BannerRundownProps) {
+  const [filterText, setFilterText] = useState("");
 
-export default class BannerTable extends React.Component<BannerRundownProps, BannerRundownState> {
-    constructor(props: BannerRundownProps) {
-        super(props);
-        this.state = {
-            filterText: '',
-        }
+  const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterText(event.target.value);
+  };
+
+  const sortByName = (r: FeaturedHistory) => r.name;
+
+  const sortByRunsLastPatch = [
+    (r: FeaturedHistory) => String(r.versions.length),
+    (r: FeaturedHistory) => versionToNumber(r.versions[r.versions.length - 1]),
+    sortByName,
+  ];
+
+  const sortByFirst = [
+    (r: FeaturedHistory) => versionToNumber(r.versions[0]),
+    sortByName,
+  ];
+
+  const sortByLast = [
+    (r: FeaturedHistory) => versionToNumber(r.versions[r.versions.length - 1]),
+    sortByName,
+  ];
+
+  const getSortFunction = () => {
+    switch (sortBy) {
+      default:
+      case "last":
+        return sortByLast;
+      case "first":
+        return sortByFirst;
+      case "runs-last":
+        return sortByRunsLastPatch;
     }
+  };
 
-    handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({filterText: event.target.value});
+  const getOrderByOrders = () => {
+    switch (sortBy) {
+      default:
+        return [
+          ...Array(getSortFunction().length - 1).fill(
+            order === "asc" ? "asc" : "desc"
+          ),
+          order === "asc" ? "desc" : "asc",
+        ];
+      case "runs-last":
+        return order === "asc"
+          ? ["asc", "desc", "desc"]
+          : ["desc", "desc", "asc"];
     }
+  };
 
-    sortByName = (r: FeaturedHistory) => r.name
+  const versionParts = getVersionParts(
+    _.chain(featuredList)
+      .map((featured) => featured.versions)
+      .flatten()
+      .value()
+  );
 
-    sortByRunsLastPatch = [
-        (r: FeaturedHistory) => String(r.versions.length),
-        (r: FeaturedHistory) => versionToNumber(r.versions[r.versions.length - 1]),
-        this.sortByName,
-    ]
+  const baseFeaturedList = _.chain(getRundowns(featuredList))
+    .orderBy(getSortFunction(), getOrderByOrders())
+    .value();
 
-    sortByFirst = [
-        (r: FeaturedHistory) => versionToNumber(r.versions[0]),
-        this.sortByName,
-    ]
+  const filteredFeaturedList = _.chain(baseFeaturedList)
+    .filter(getFilterFunction(filterText))
+    .value();
 
-    sortByLast = [
-        (r: FeaturedHistory) => versionToNumber(r.versions[r.versions.length - 1]),
-        this.sortByName,
-    ]
+  const rundown = filteredFeaturedList.length
+    ? filteredFeaturedList
+    : baseFeaturedList;
+  return (
+    <>
+      <Table definition unstackable selectable compact className={"history"}>
+        <HistoryHeader
+          versionParts={versionParts}
+          onChange={handleFilterChange}
+        />
+        <Table.Body>
+          {rundown.map((r, rI) => (
+            <HistoryRow
+              key={rI}
+              bannerType={bannerType}
+              featuredList={featuredList}
+              rundown={r}
+              dataset={dataset}
+            />
+          ))}
+        </Table.Body>
 
-    getSortFunction = () => {
-        switch (this.props.sortBy) {
-            default:
-            case 'last':
-                return this.sortByLast
-            case 'first':
-                return this.sortByFirst
-            case 'runs-last':
-                return this.sortByRunsLastPatch
-        }
-    }
-
-    getOrderByOrders = () => {
-        switch (this.props.sortBy) {
-            default:
-                return [
-                    ...Array(this.getSortFunction().length - 1).fill(this.props.order === 'asc' ? 'asc' : 'desc'),
-                    this.props.order === 'asc' ? 'desc' : 'asc',
-                ]
-            case 'runs-last':
-                return this.props.order === 'asc' ? ['asc', 'desc', 'desc'] : ['desc', 'desc', 'asc']
-        }
-    }
-
-    render() {
-        let {bannerType, featuredList, dataset} = this.props
-        const versionParts = getVersionParts(
-            _.chain(featuredList)
-                .map((featured) => featured.versions)
-                .flatten()
-                .value()
-        )
-
-        const baseFeaturedList = _.chain(getRundowns(featuredList))
-            .orderBy(this.getSortFunction(), this.getOrderByOrders())
-            .value()
-
-        const filteredFeaturedList = _.chain(baseFeaturedList)
-            .filter(getFilterFunction(this.state.filterText))
-            .value()
-
-        const rundown = filteredFeaturedList.length ? filteredFeaturedList : baseFeaturedList
-        return (
-            <>
-                <Table definition unstackable selectable compact className={'history'}>
-                    <HistoryHeader
-                        versionParts={versionParts}
-                        onChange={this.handleFilterChange}
-                    />
-                    <Table.Body>
-                        {rundown.map((r, rI) => <HistoryRow key={rI}
-                                                            bannerType={bannerType}
-                                                            featuredList={featuredList}
-                                                            rundown={r}
-                                                            dataset={dataset}
-                            />
-                        )}
-                    </Table.Body>
-
-                    <HistoryFooter versionParts={versionParts}/>
-                </Table>
-            </>
-        );
-    }
+        <HistoryFooter versionParts={versionParts} />
+      </Table>
+    </>
+  );
 }
