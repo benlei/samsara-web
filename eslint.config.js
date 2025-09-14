47b025daf106a5b9336c242990029537a784f772
Benjamin Lei <9943953+benlei@users.noreply.github.com>
2025-09-14 08:23:08 +0000

    maybe ai fixes

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
-const { FlatCompat } = require('@eslint/eslintrc');
+try {
+  const { FlatCompat } = require('@eslint/eslintrc');
+  
+  const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    resolvePluginsRelativeTo: __dirname,
+  });
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-module.exports = [
-  ...compat.extends('next/core-web-vitals'),
-];
+  module.exports = [
+    ...compat.extends('next/core-web-vitals'),
+  ];
+} catch (error) {
+  // Fallback configuration if @eslint/eslintrc is not available
+  module.exports = {
+    extends: ['next/core-web-vitals'],
+  };
+}
